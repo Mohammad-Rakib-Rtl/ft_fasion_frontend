8a2ebc009ce78d2adcad499d0ae7b60c20c85174
Mohammad-Rakibuzzaman <roman1117459@gmail.com>
2026-01-21 02:53:35 +0800

    add full frontend part in js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/products/';
+const API_URL = 'https://web-production-1f7f.up.railway.app/api/products/';
 
 export async function getProducts() {
   const res = await axios.get(API_URL);
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -27,7 +27,7 @@ function Cart() {
   };
 
   try {
-    const res = await fetch("http://127.0.0.1:8000/api/checkout/", {
+    const res = await fetch("https://web-production-1f7f.up.railway.app/api/checkout/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(orderData),
